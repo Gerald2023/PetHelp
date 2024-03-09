eba67713d62aab78760568edc79fb48c3c30479e
Gerald2023 <123606370+Gerald2023@users.noreply.github.com>
2024-03-09 14:31:37 -0600

    added Web project

---
+++ GV.Pet_Help/GV.Pet_Help.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GV.Pet_Help.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
